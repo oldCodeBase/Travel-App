c081d07847ac7bdd7ffb47786fb274db22e3bd27
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-01-28 11:10:58 +0300

    Initial Commit

---
+++ Travel App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travel App
+//
+//  Created by Ibragim Akaev on 28/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travel App/Travel_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_AppApp.swift
+//  Travel App
+//
+//  Created by Ibragim Akaev on 28/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Travel_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
