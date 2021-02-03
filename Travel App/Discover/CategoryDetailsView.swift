c5e9487a895ccc3e700073d9f0d7b69d886787b2
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-02-03 16:45:45 +0300

    Dependency Injection and add network error

--- Travel App/Discover/CategoryDetailsView.swift
+++ Travel App/Discover/CategoryDetailsView.swift
@@ -15,15 +15,23 @@ class CategoryDetailsViewModel: ObservableObject {
     
     @Published var errorMessage = ""
     
-    init() {
+    init(name: String) {
         
-        guard let url = URL(string: "https://travel.letsbuildthatapp.com/travel_discovery/category?name=art") else { return }
+        let urlString = "https://travel.letsbuildthatapp.com/travel_discovery/category?name=\(name.lowercased())".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        guard let url = URL(string: urlString) else {
+            self.isLoading = false
+            return
+        }
         
         URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            if let statusCode = (resp as? HTTPURLResponse)?.statusCode, statusCode >= 400 {
+                self.isLoading = false
+                self.errorMessage = "Bad status: \(statusCode)"
+                return
+            }
             
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                
-                
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 guard let data = data else { return }
                 
                 do {
@@ -41,7 +49,13 @@ class CategoryDetailsViewModel: ObservableObject {
 
 struct CategoryDetailsView: View {
     
-    @ObservedObject var vm = CategoryDetailsViewModel()
+    private let name: String
+    @ObservedObject private var vm: CategoryDetailsViewModel
+    
+    init(name: String) {
+        self.name = name
+        self.vm = .init(name: name)
+    }
     
     var body: some View {
         ZStack {
@@ -58,12 +72,20 @@ struct CategoryDetailsView: View {
                 
             } else {
                 ZStack {
-                    Text(vm.errorMessage)
+                    
+                    if !vm.errorMessage.isEmpty {
+                        VStack(spacing: 12) {
+                            Image(systemName: "xmark.octagon.fill")
+                                .font(.system(size: 64, weight: .semibold))
+                                .foregroundColor(.red)
+                            Text(vm.errorMessage)
+                        }
+                    }
+                    
                     ScrollView {
                         ForEach(vm.places, id: \.self) { place in
                             VStack(alignment: .leading, spacing: 0) {
                                 KFImage(URL(string: place.thumbnail))
-                                    .fade(duration: 0.5)
                                     .resizable()
                                     .scaledToFill()
                                 
@@ -79,14 +101,14 @@ struct CategoryDetailsView: View {
                 
             }
         }
-        .navigationBarTitle("Category", displayMode: .inline)
+        .navigationBarTitle(name, displayMode: .inline)
     }
 }
 
 struct CategoryDetailsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            CategoryDetailsView()
+            CategoryDetailsView(name: "Food")
         }
     }
 }
--- Travel App/Discover/DiscoverCategoriesView.swift
+++ Travel App/Discover/DiscoverCategoriesView.swift
@@ -22,7 +22,7 @@ struct DiscoverCategoriesView: View {
             HStack(alignment: .top, spacing: 14) {
                 ForEach(categories, id: \.self) { category in
                     NavigationLink(
-                        destination: CategoryDetailsView(),
+                        destination: CategoryDetailsView(name: category.name),
                         label: {
                             VStack(spacing: 8) {
                                 Image(systemName: category.imageName)
@@ -46,7 +46,7 @@ struct DiscoverCategoriesView: View {
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            CategoryDetailsView()
+            CategoryDetailsView(name: "Art")
         }
         DiscoverView()
     }
