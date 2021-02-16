c0c23e785f31849fe52c9e26d4aa21ed65253dbc
Ibraczar <44230748+Ibraczar@users.noreply.github.com>
2021-02-16 22:37:21 +0300

    Created DestinationDetails model

---
+++ Travel App/Models/DestinationDetails.swift
@@ -0,0 +1,13 @@
+//
+//  DestinationDetails.swift
+//  Travel App
+//
+//  Created by Ibragim Akaev on 16/02/2021.
+//
+
+import Foundation
+
+struct DestinationDetails: Decodable {
+    let description: String
+    let photos: [String]
+}
